ebb3b99efe57cc6ee4397f964f63a1e019eb4f67
Prospero1988 <arek.kein@gmail.com>
2024-10-18 18:10:37 +0200

    playing with DNN

---
+++ DNN/tags_config_pytorch.py
@@ -0,0 +1,18 @@
+# tags_config.py
+
+mlflow_tags1 = {
+    "architecture": "Pytorch",
+    "model": "DNN",
+    "stage": "Optuna HP",
+    "author": "aleniak",
+    "property": "CHIlogD"
+}
+
+mlflow_tags2 = {
+    "architecture": "Pytorch",
+    "predictor": "1H",
+    "model": "DNN",
+    "stage": "training",
+    "author": "aleniak",
+    "property": "CHIlogD"
+}
---
+++ DNN/tags_config_tensor.py
@@ -0,0 +1,18 @@
+# tags_config.py
+
+mlflow_tags1 = {
+    "architecture": "TensorFlow",
+    "model": "DNN",
+    "stage": "Optuna HP",
+    "author": "aleniak",
+    "property": "CHIlogD"
+}
+
+mlflow_tags2 = {
+    "architecture": "TensorFlow",
+    "predictor": "1H",
+    "model": "DNN",
+    "stage": "training",
+    "author": "aleniak",
+    "property": "CHIlogD"
+}
